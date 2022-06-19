0329388d870b0e8b4e209a181c4b12ee52b3d561
Dazzler123 <dasinduhewagamage@gmail.com>
2022-06-19 20:38:10 +0530

    User controls modified

--- src/controller/UserControlsFormController.java
+++ src/controller/UserControlsFormController.java
@@ -8,12 +8,29 @@ import util.UINavigation;
 import java.io.IOException;
 
 public class UserControlsFormController {
-    public void btnEditUnPw(ActionEvent actionEvent) throws IOException {
+
+    public void btnToMakeRegistration(ActionEvent actionEvent) {
+    }
+
+    public void btnToManageStudents(ActionEvent actionEvent) {
+    }
+
+    public void btnToManageRooms(ActionEvent actionEvent) {
+    }
+
+    public void btnToEditUnPw(ActionEvent actionEvent) throws IOException {
         UINavigation.setUI("EditUnPwdForm","Edit Username or Password");
+    }
+
+    public void btnKeyMoneyNotPaid(ActionEvent actionEvent) {
+    }
+
+    public void btnStudentDetails(ActionEvent actionEvent) {
     }
 
     public void btnExit(ActionEvent actionEvent) {
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         stage.close();
     }
+
 }
