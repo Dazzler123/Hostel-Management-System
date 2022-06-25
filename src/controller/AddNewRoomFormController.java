86c7a69f1c37b43531f131f851d17551ce40d814
Dazzler123 <dasinduhewagamage@gmail.com>
2022-06-25 16:01:53 +0530

    Initialized AddNewRoom form

--- src/controller/AddNewRoomFormController.java
+++ src/controller/AddNewRoomFormController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class AddNewRoomFormController {
+    public JFXTextField txtRoomID;
+    public JFXTextField txtRoomType;
+    public JFXTextField txtKeyMoney;
+    public JFXTextField txtRoomQty;
+
+    public void btnSave(ActionEvent actionEvent) {
+    }
+
+    public void btnExit(ActionEvent actionEvent) {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.close();
+    }
 }
