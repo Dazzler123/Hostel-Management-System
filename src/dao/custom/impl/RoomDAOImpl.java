84ff2e1a3165d0bb3291cda7c76096b7920b2014
Dazzler123 <dasinduhewagamage@gmail.com>
2022-06-26 14:49:27 +0530

    delete() function in RoomDAO updated

--- src/dao/custom/impl/RoomDAOImpl.java
+++ src/dao/custom/impl/RoomDAOImpl.java
@@ -60,7 +60,7 @@ public class RoomDAOImpl implements RoomDAO {
         Transaction transaction = session.beginTransaction();
 
         Query query = session.createQuery("FROM room WHERE room_id = : id");
-        query.setParameter("id",id);
+        query.setParameter("id", id);
         List<Room> list = query.list();
 
         transaction.commit();
@@ -87,8 +87,8 @@ public class RoomDAOImpl implements RoomDAO {
         Transaction transaction = session.beginTransaction();
 
         Query query = session.createQuery("UPDATE room SET type = : r_type, key_money = : r_keyMoney, qty = : r_qty WHERE room_id = : r_id");
-        query.setParameter("r_type",room.getType());
-        query.setParameter("r_keyMoney",room.getKey_money());
+        query.setParameter("r_type", room.getType());
+        query.setParameter("r_keyMoney", room.getKey_money());
         query.setParameter("r_qty", room.getQty());
         query.setParameter("r_id", room.getRoom_id());
         int isUpdated = query.executeUpdate();
@@ -96,9 +96,9 @@ public class RoomDAOImpl implements RoomDAO {
         transaction.commit();
         session.close();
 
-        if(isUpdated > 0)
+        if (isUpdated > 0)
             return true;
-        else{
+        else {
             return false;
         }
     }
@@ -107,17 +107,26 @@ public class RoomDAOImpl implements RoomDAO {
         Session session = FactoryConfiguration.getInstance().getSession();
         Transaction transaction = session.beginTransaction();
 
+        Room room = session.get(Room.class, id);
+
+        Query query1 = session.createQuery("DELETE FROM Reserve WHERE room_id = : r");
+        query1.setParameter("r", room);
+        int isDetached = query1.executeUpdate();
+
         Query query = session.createQuery("DELETE FROM room WHERE room_id = : id");
-        query.setParameter("id",id);
+        query.setParameter("id", id);
         int isDeleted = query.executeUpdate();
 
         transaction.commit();
         session.close();
-        
-        if(isDeleted > 0){
-            return true;
-        }else{
-            return false;
+
+        if (isDetached > 0) {
+            if (isDeleted > 0) {
+                return true;
+            }
         }
+        return false;
     }
+
+
 }
