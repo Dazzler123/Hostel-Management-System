f55bd746c0524b0173d95d4c8c489f4d0bfd670a
Dazzler123 <dasinduhewagamage@gmail.com>
2022-06-26 16:30:44 +0530

    Implemented DAOFactory

--- src/dao/DAOFactory.java
+++ src/dao/DAOFactory.java
@@ -1,4 +1,36 @@
 package dao;
 
+import dao.custom.impl.*;
+
 public class DAOFactory {
+    private static DAOFactory daoFactory;
+
+    private DAOFactory() {
+
+    }
+
+    public static DAOFactory getDAOFactory() {
+        return daoFactory == null ? daoFactory = new DAOFactory() : daoFactory;
+    }
+
+    public enum DAOTypes {
+        STUDENT, ROOM, RESERVE, USER_CREDENTIAL, QUERY
+    }
+
+    public SuperDAO getDAO(DAOTypes types) {
+        switch (types) {
+            case STUDENT:
+                return new StudentDAOImpl();
+            case ROOM:
+                return new RoomDAOImpl();
+            case RESERVE:
+                return new ReserveDAOImpl();
+            case USER_CREDENTIAL:
+                return new UserCredentialDAOImpl();
+            case QUERY:
+                return new QueryDAOImpl();
+            default:
+                return null;
+        }
+    }
 }
