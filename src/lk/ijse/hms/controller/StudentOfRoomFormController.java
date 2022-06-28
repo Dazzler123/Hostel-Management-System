218f26ee86ab2d1afdc2cb19dd46055efb334d37
Dazzler123 <dasinduhewagamage@gmail.com>
2022-06-28 21:04:55 +0530

    Initialized details of student's who reserved the specific room UI and its Controller class

--- src/lk/ijse/hms/controller/StudentOfRoomFormController.java
+++ src/lk/ijse/hms/controller/StudentOfRoomFormController.java
@@ -1,4 +1,23 @@
 package lk.ijse.hms.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class StudentOfRoomFormController {
+    public TableView tblStudent;
+    public TableColumn colID;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colContactNo;
+    public TableColumn colDOB;
+    public TableColumn colGender;
+    public JFXTextField txtRoomID;
+
+    public void txtSearchRoom(ActionEvent actionEvent) {
+    }
+
+    public void btnExit(ActionEvent actionEvent) {
+    }
 }
